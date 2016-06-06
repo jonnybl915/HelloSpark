a0611468b181b38d9fafd6ee8ce6cccdcf0284df
jonathandavidblack <jonnybl915@gmail.com>
2016-06-06 11:44:46 -0400

    initial import: created html post and get 6.6.16

---
+++ src/com/theironyard/jdblack/User.java
@@ -0,0 +1,12 @@
+package com.theironyard.jdblack;
+
+/**
+ * Created by jonathandavidblack on 6/6/16.
+ */
+public class User {
+    String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+}
