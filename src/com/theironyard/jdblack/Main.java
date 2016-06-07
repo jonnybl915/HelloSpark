a5287ff932ea8f5f582582379c2a314b03619af2
jonathandavidblack <jonnybl915@gmail.com>
2016-06-07 10:27:46 -0400

    add session

--- src/com/theironyard/jdblack/Main.java
+++ src/com/theironyard/jdblack/Main.java
@@ -1,6 +1,7 @@
 package com.theironyard.jdblack;
 
 import spark.ModelAndView;
+import spark.Session;
 import spark.Spark;
 import spark.template.mustache.MustacheTemplateEngine;
 
@@ -9,7 +10,8 @@ import java.util.HashMap;
 
 public class Main {
 
-    static User user;
+    //static User user; changing this to a hashmap, then go through
+    static HashMap<String, User> users = new HashMap<>();
     static ArrayList<User> userList = new ArrayList<>();
 
     public static void main(String[] args) {
@@ -17,12 +19,15 @@ public class Main {
         Spark.get(
                 "/",
                 (request, response) -> {
+                    Session session = request.session(); //pulling info from previous session
+                    String username = session.attribute("username");
+
                     HashMap map = new HashMap();
-                    if (user == null){
+                    if (username == null){ //changed from user to username
                         return new ModelAndView(map, "login.html");
                     }
                     else {
-                        map.put("name", user.name);
+                        map.put("name", username); //changed from user.name to username
                         map.put("users", userList);
                         return new ModelAndView(map, "home.html");
                     }
@@ -33,16 +38,26 @@ public class Main {
                 "/login",
                 (request, response) -> {
                     String username = request.queryParams("username");
-                    user = new User(username);
-                    userList.add(user);
+                    User user = users.get(username);
+                    if (user == null) {
+                        user = new User(username);
+                        userList.add(user);
+                        users.put(username, user); //added this line
+                    }
+                    //always create session in the "login" route
+                    Session session = request.session();
+                    session.attribute("username", username); //acts as a special hashmap to store user info
+
                     response.redirect("/");
                     return ""; //doesn't have any effect but this is necessary to satisfy the
                 }
         );
         Spark.post(
           "/logout",
-                (request, response) -> {
-                    user = null;
+                (request, response) -> { //removed commented section
+                    Session session = request.session();
+                    session.invalidate();
+                    //user = null;
                     response.redirect("/");
                     return "";
                 }
